d5745893300151a96b15bed6309603b452a3d432
Bruno Alla <bruno.alla@founders4schools.org.uk>
2016-12-06 23:36:44 +0000

    Replace test script by test settings and manage.py

---
+++ manage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
