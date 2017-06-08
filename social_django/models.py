863a3e217b5597c82e7c98ffb0a128e5170a2a86
Michal Čihař <michal@cihar.com>
2017-06-08 08:55:29 +0200

    Add timestamp to Code model
    This will allow to delete old not verified codes or check code age
    before verification.

--- social_django/models.py
+++ social_django/models.py
@@ -109,6 +109,7 @@ class Code(models.Model, DjangoCodeMixin):
     email = models.EmailField(max_length=EMAIL_LENGTH)
     code = models.CharField(max_length=32, db_index=True)
     verified = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
         app_label = "social_django"
