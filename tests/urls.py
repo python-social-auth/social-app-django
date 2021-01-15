29072f9111c4aca1fac0d6f4fd7e102981ee6c81
Michal Čihař <michal@cihar.com>
2021-01-15 13:45:57 +0100

    Replace deprecated urls.url with urls.path

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8
-from django.conf.urls import url, include
+from django.urls import include, path
 from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('social_django.urls', namespace='social')),
+    path('admin/', admin.site.urls),
+    path('', include('social_django.urls', namespace='social')),
 ]
