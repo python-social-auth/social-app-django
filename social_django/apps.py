cafdafc7aed9fd2985ad9199662e1daff81c1ca9
Janne Rönkkö <janne.ronkko@iki.fi>
2021-05-21 14:41:42 +0300

    Explicitly set auto field type
    ..to avoid unexisting migrations with Django 3.2
    
    See
    https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
    
    Fix #313

--- social_django/apps.py
+++ social_django/apps.py
@@ -2,6 +2,8 @@ from django.apps import AppConfig
 
 
 class PythonSocialAuthConfig(AppConfig):
+    # Explicitly set default auto field type to avoid migrations in Django 3.2+
+    default_auto_field = 'django.db.models.AutoField'
     # Full Python path to the application eg. 'django.contrib.admin'.
     name = 'social_django'
     # Last component of the Python path to the application eg. 'admin'.
