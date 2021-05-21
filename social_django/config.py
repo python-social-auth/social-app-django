de9565c8b94caef7e8ea4b4f7e3cb003c7e890d1
Janne Rönkkö <janne.ronkko@iki.fi>
2021-05-21 14:29:20 +0300

    Fix deprecation warning about default_app_config from Django 3.2+
    The PythonSocialAuthConfig class is moved to apps module so that
    Django finds it automatically.
    
    See
    https://docs.djangoproject.com/en/3.2/releases/3.2/#automatic-appconfig-discovery
    
    Fix #311

--- social_django/config.py
+++ social_django/config.py
@@ -1,10 +1,2 @@
-from django.apps import AppConfig
-
-
-class PythonSocialAuthConfig(AppConfig):
-    # Full Python path to the application eg. 'django.contrib.admin'.
-    name = 'social_django'
-    # Last component of the Python path to the application eg. 'admin'.
-    label = 'social_django'
-    # Human-readable name for the application eg. "Admin".
-    verbose_name = 'Python Social Auth'
+# For backward compatibility. You should use the configuration from apps module
+from .apps import PythonSocialAuthConfig  # noqa: F401
