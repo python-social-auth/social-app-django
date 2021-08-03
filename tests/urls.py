bf140b76f1e9e820228306dcd5756c25169a94c2
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-03 14:03:10 +0800

    Delete tests directory

--- tests/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('social_django.urls', namespace='social')),
-]
