788e2b000107a336b2971ea5c5de79a582ab5b84
Ben Rogers-Newsome <>
2021-09-27 17:36:16 +0100

    RD-840 - Fixed tests

--- social_django/audit/clients.py
+++ social_django/audit/clients.py
@@ -1,4 +1,3 @@
-
 class AbstractBaseAuditLogger(object):
 
     @classmethod
@@ -24,3 +23,30 @@ class AbstractBaseAuditLogger(object):
     @classmethod
     def log_decrypt_token_event(cls, source, user_id, token, **kwargs):
         raise NotImplementedError('Implement in subclass')
+
+
+class DummyAuditLogger(AbstractBaseAuditLogger):
+
+    @classmethod
+    def log_login_event(cls, source, user_id, request=None, **kwargs):
+        pass
+
+    @classmethod
+    def log_delete_account_event(cls, source, user_id, request=None, **kwargs):
+        pass
+
+    @classmethod
+    def log_request_token_event(cls, source, user_id, token, **kwargs):
+        pass
+
+    @classmethod
+    def log_revoke_token_event(cls, source, user_id, token, **kwargs):
+        pass
+
+    @classmethod
+    def log_encrypt_token_event(cls, source, user_id, token, **kwargs):
+        pass
+
+    @classmethod
+    def log_decrypt_token_event(cls, source, user_id, token, **kwargs):
+        pass
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -17,14 +17,8 @@ class SocialAdminTest(TestCase):
         )
 
     def test_admin_app_name(self):
-        """The App name in the admin index page"""
+        """App should not be registered in admin panel"""
         self.client.login(username='admin', password='super-duper-test')
         response = self.client.get(reverse('admin:index'))
-        self.assertContains(response, "Python Social Auth")
+        self.assertNotContains(response, "Python Social Auth")
 
-    def test_social_auth_changelist(self):
-        """The App name in the admin index page"""
-        self.client.login(username='admin', password='super-duper-test')
-        meta = UserSocialAuth._meta
-        url_name = 'admin:%s_%s_changelist' % (meta.app_label, meta.model_name)
-        self.client.get(reverse(url_name))
