369cd21a7528210d5cdf60b2b131da66d9e23406
Michal Čihař <michal@cihar.com>
2023-01-31 08:24:53 +0100

    deps: Update supported Python versions
    - add 3.10 and 3.11
    - drop 3.6

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ],
     long_description=long_description(),
     long_description_content_type="text/markdown",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=load_requirements(),
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -50,10 +50,11 @@ setup(
         "Environment :: Web Environment",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     zip_safe=False,
 )
